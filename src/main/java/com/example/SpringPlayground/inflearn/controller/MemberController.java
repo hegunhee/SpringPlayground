1642ec0a106ea42d50f74b30fa61ca8690949c47
hegunhee <gunhee0072@gmail.com>
2024-05-03 20:01:02 +0900

    [inflearn] feat : 회원 등록 기능 추가

--- src/main/java/com/example/SpringPlayground/inflearn/controller/MemberController.java
+++ src/main/java/com/example/SpringPlayground/inflearn/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.SpringPlayground.inflearn.controller;
 
+import com.example.SpringPlayground.inflearn.domain.Member;
 import com.example.SpringPlayground.inflearn.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,30 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    /**
+     *
+     * @return 값에 해당하는 tamplate의 html을 조회
+     */
+    @GetMapping("members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    /**
+     * POST이므로 데이터를 등록해줌 form action이 해당 url
+     * MemberForm의 name 값이 들어옴
+     * @param form
+     * @return
+     */
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
