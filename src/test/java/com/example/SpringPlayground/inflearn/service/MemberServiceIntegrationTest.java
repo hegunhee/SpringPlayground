2b716dec1f23b9d72a12908b62e0b6dfe6802a63
hegunhee <gunhee0072@gmail.com>
2024-05-05 19:49:57 +0900

    [inflearn] test : JDBC Spring Test

--- src/test/java/com/example/SpringPlayground/inflearn/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/example/SpringPlayground/inflearn/service/MemberServiceIntegrationTest.java
@@ -1,2 +1,54 @@
-package com.example.SpringPlayground.inflearn.service;public class MemberServiceIntegrationTest {
+package com.example.SpringPlayground.inflearn.service;
+
+import com.example.SpringPlayground.inflearn.domain.Member;
+import com.example.SpringPlayground.inflearn.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * TEST는 반복 가능해아 한다.
+ * Transactional어노테이션을 사용하면 각각의 테스트 진행 후에 db를 롤백시킨다.(반영시키지않는다)
+ */
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = org.junit.jupiter.api.Assertions.assertThrows(IllegalStateException.class,() -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
 }
