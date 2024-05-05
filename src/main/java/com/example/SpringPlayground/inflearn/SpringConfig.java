404a98739215992528b7de8cf82fcb80364efc8f
hegunhee <gunhee0072@gmail.com>
2024-05-05 18:25:23 +0900

    [inflearn] feat : JDBC 실습 (다형성)

--- src/main/java/com/example/SpringPlayground/inflearn/SpringConfig.java
+++ src/main/java/com/example/SpringPlayground/inflearn/SpringConfig.java
@@ -1,16 +1,26 @@
 package com.example.SpringPlayground.inflearn;
 
+import com.example.SpringPlayground.inflearn.repository.JdbcMemberRepository;
 import com.example.SpringPlayground.inflearn.repository.MemberRepository;
-import com.example.SpringPlayground.inflearn.repository.MemoryMemberRepository;
 import com.example.SpringPlayground.inflearn.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 직접 컴포넌트에 의존성을 설정하는 방법
  */
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -19,6 +29,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/test/java/com/example/SpringPlayground/inflearn/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,2 @@
+package com.example.SpringPlayground.inflearn.service;public class MemberServiceIntegrationTest {
+}
