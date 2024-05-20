16a185fbf302646ed623063a9a78fe2e3da8d4ba
hegunhee <gunhee0072@gmail.com>
2024-05-20 20:54:45 +0900

    [inflearn-jpa-1] feat : 도메인 설계시 주의점

--- src/main/java/com/example/SpringPlayground/inflearn/jpa/shop/domain/Order.java
+++ src/main/java/com/example/SpringPlayground/inflearn/jpa/shop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) // 연관된 query까지 다 날라옴
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) // 나머지 OrderItem도 자동으로 등록된다
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,5 +32,29 @@ public class Order {
 
     @Enumerated
     private OrderStatus status; // 주문상태 [ORDER, CANCALED]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    public static void main(String[] args) {
+        Member member = new Member();
+        Order order = new Order();
+        member.getOrders().add(order);
+        order.setMember(member);
+
+    }
 
 }
