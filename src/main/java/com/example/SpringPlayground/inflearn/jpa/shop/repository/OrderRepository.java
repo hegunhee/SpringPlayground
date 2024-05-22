7da6ffd4bd232399cbf1f9e69cda10850759566c
hegunhee <gunhee0072@gmail.com>
2024-05-22 15:43:45 +0900

    [inflearn-jpa-1] feat : 주문 검색 기능 개발

--- src/main/java/com/example/SpringPlayground/inflearn/jpa/shop/repository/OrderRepository.java
+++ src/main/java/com/example/SpringPlayground/inflearn/jpa/shop/repository/OrderRepository.java
@@ -2,9 +2,13 @@ package com.example.SpringPlayground.inflearn.jpa.shop.repository;
 
 import com.example.SpringPlayground.inflearn.jpa.shop.domain.Order;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,7 +25,32 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return findAllByCriteria(orderSearch);
+    }
+
+    /**
+     * JPA Criteria
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        ArrayList<Predicate> criteria = new ArrayList<>();
+
+        if(orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
 }
