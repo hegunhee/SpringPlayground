f15897d784f16b6904054933faaea2ea1844f7dd
hegunhee <gunhee0072@gmail.com>
2024-05-24 22:01:09 +0900

    [Todo-Service] feat : TodoService 코드 작성

---
+++ src/main/java/com/example/SpringPlayground/todoServer/service/TodoService.java
@@ -0,0 +1,50 @@
+package com.example.SpringPlayground.todoServer.service;
+
+import com.example.SpringPlayground.todoServer.domain.Todo;
+import com.example.SpringPlayground.todoServer.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    @Transactional
+    public String save(String todoText) {
+        Todo todo = Todo.createTodo(todoText);
+        todoRepository.save(todo);
+        return todoText;
+    }
+
+    public Todo findOne(String todoId) {
+        return todoRepository.findOne(todoId);
+    }
+
+    public List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
+
+    @Transactional
+    public String delete(Todo todo) {
+        todoRepository.delete(todo);
+        return todo.getText();
+    }
+
+    @Transactional
+    public void deleteAll() {
+        todoRepository.deleteAll();
+    }
+
+    @Transactional
+    public String toggleTodo(String todoId) {
+        Todo todo = todoRepository.findOne(todoId);
+        todo.toggleTodo();
+        return todo.getText();
+    }
+}
