39fed048883363da48c7b9ec1d96231657bf07f9
hegunhee <gunhee0072@gmail.com>
2024-05-24 20:36:02 +0900

    [Todo-Server] feat : DB Repository 작성

--- src/main/java/com/example/SpringPlayground/todoServer/TodoApplication.java
+++ src/main/java/com/example/SpringPlayground/todoServer/TodoApplication.java
@@ -1,2 +1,12 @@
-package com.example.SpringPlayground.todoServer;public class TodoApplication {
+package com.example.SpringPlayground.todoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 }
