cc3390107d638034716cdcddb10c85663cfbab9b
이건희 <57277631+hegunhee@users.noreply.github.com>
2024-06-27 09:54:24 +0900

    Revert "[Todo-Server] fix : save를 하고 todoId값을 넘겼을때 오류가 나타나던 문제 수정"

--- src/main/java/com/example/SpringPlayground/todoServer/controller/TodoController.java
+++ src/main/java/com/example/SpringPlayground/todoServer/controller/TodoController.java
@@ -3,26 +3,21 @@ package com.example.SpringPlayground.todoServer.controller;
 import com.example.SpringPlayground.todoServer.domain.Todo;
 import com.example.SpringPlayground.todoServer.service.TodoService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/todo",produces="application/json")
+@RequestMapping("/todo")
 @RequiredArgsConstructor
-@Slf4j
 public class TodoController {
 
     private final TodoService todoService;
 
     @PostMapping
-    TodoForm save(@RequestBody TodoForm todoId) {
-        todoService.save(todoId.getTodoId());
-        return todoId;
+    String save(@RequestBody TodoForm todoId) {
+        return todoService.save(todoId.getTodoId());
     }
 
     @GetMapping
