8c7f66ad04643bad4fcc85ad1c3720f3daa57c5b
hegunhee <gunhee0072@gmail.com>
2024-07-04 08:23:18 +0900

    [spring-mvc] feat : http 응답 요청

---
+++ src/main/java/com/example/SpringPlayground/inflearn/spring/mvc1/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,76 @@
+package com.example.SpringPlayground.inflearn.spring.mvc1.basic.request;
+
+import com.example.SpringPlayground.inflearn.spring.mvc1.basic.HelloData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {"username":"hello", "age":20}
+ * content-type : application/json
+ */
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}",messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v2")
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws IOException {
+
+        log.info("messageBody={}",messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v3")
+    public String requestBodyJsonV3(@RequestBody HelloData helloData) throws IOException {
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4") // 만약 HelloData 포맷대로 들어오지 않는다면 기본값이 지정됨 (string - null, int - 0)
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) throws IOException {
+        HelloData data = httpEntity.getBody();
+        log.info("username={}, age={}",data.getUsername(),data.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV4(@RequestBody HelloData data) throws IOException {
+        log.info("username={}, age={}",data.getUsername(),data.getAge());
+
+        return data;
+    }
+}
