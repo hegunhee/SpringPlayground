04dacb2369fae71e9028df291570eb0e5da8b785
hegunhee <gunhee0072@gmail.com>
2024-07-04 20:49:00 +0900

    [spring-mvc] feat : 웹 예제

---
+++ src/main/java/com/example/SpringPlayground/inflearn/spring/mvcweb/itemservice/ItemServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringPlayground.inflearn.spring.mvcweb.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class,args);
+    }
+}
