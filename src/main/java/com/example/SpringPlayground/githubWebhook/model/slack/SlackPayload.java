ee65fb87561f97a82ee9e0069e7349aab9cfdebc
hegunhee <gunhee0072@gmail.com>
2024-07-20 20:42:05 +0900

    [slack-webhook] refactor : slackPayload 클래스 롬북 어노테이션 수정, 블럭 리스트 방어적복사

--- src/main/java/com/example/SpringPlayground/githubWebhook/model/slack/SlackPayload.java
+++ src/main/java/com/example/SpringPlayground/githubWebhook/model/slack/SlackPayload.java
@@ -1,23 +1,26 @@
 package com.example.SpringPlayground.githubWebhook.model.slack;
 
 import com.example.SpringPlayground.githubWebhook.model.slack.component.SlackPayloadComponent;
-import lombok.Data;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@ToString
 public class SlackPayload {
 
-    public SlackPayload() {
-        this.blocks = new ArrayList();
-    }
-
     // JsonProperty가 적용되지 않음
-    private List<SlackPayloadComponent> blocks;
+    private final List<SlackPayloadComponent> blocks;
+
+    public SlackPayload() {
+        this.blocks = new ArrayList<>();
+    }
 
     public void addComponent(SlackPayloadComponent slackPayloadComponent) {
         this.blocks.add(slackPayloadComponent);
     }
 
+    public List<SlackPayloadComponent> getBlocks() {
+        return new ArrayList<>(blocks);
+    }
 }
