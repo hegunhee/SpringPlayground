f274670bca259092c570360b22387fd72471fe9d
hegunhee <gunhee0072@gmail.com>
2024-07-31 11:33:52 +0900

    [spring-db1] feat : jdbc 실습

---
+++ jdbc/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    static final String USERNAME = "sa";
+    static final String PW = "";
+}
