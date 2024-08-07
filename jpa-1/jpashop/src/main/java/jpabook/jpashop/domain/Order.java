a708f59be511b6dce1f4d4ff4a3ffd9b1f2d8d87
hegunhee <gunhee0072@gmail.com>
2024-08-07 18:11:08 +0900

    [jpa-1] feat : 주문 도메인 설계

--- jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -10,10 +10,12 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
@@ -21,10 +23,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) // Order만 저장했을때 해당되는 OrderItem을 자동으로 저장
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // Order만 저장했을때 해당되는 OrderItem을 자동으로 저장
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -48,5 +50,47 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //JPA는 protected까지는 접근 가능하므로 정적 팩토리 메서드를 사용한다면 protected로
+    protected Order() {
+
+    }
+
+    //== 생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem item : orderItems) {
+            order.addOrderItem(item);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpa-1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,4 +23,28 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    protected OrderItem() {
+
+    }
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    /**
+     * 비즈니스 로직
+     */
+    public void cancel() {
+        item.addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 }
