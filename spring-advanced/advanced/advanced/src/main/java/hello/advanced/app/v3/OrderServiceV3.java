be3bb378cfbf6dc6cbf3c8b28c7fd95a0dfe92b6
hegunhee <gunhee0072@gmail.com>
2024-08-17 13:23:02 +0900

    [spring-advanced] feat : ThreadLocal

---
+++ spring-advanced/advanced/advanced/src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service // 내부에 컴포넌트 어노테이션이 있어서 컴포넌트 스캔의 대상 자동으로 빈 생성됨
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.request()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status,e);
+            throw e;
+        }
+    }
+}
