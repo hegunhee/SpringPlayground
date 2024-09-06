3ed99f28aa513d7c5d0040b5e173315c566cf18b
hegunhee <gunhee0072@gmail.com>
2024-09-06 15:24:56 +0900

    [querydsl] feat : 도메인 모델 설계

---
+++ querydsl/querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,35 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    protected Team(String teamA) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
